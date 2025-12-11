8af6ab71de939c56b5561d6518667d3f204acc75
Morris <1216770@student.roc-nijmegen.nl>
2025-12-11 14:25:18 +0100

    Add loading page and initial JavaScript file for Volare Airways
    - Created loading.php with basic HTML structure and linked CSS and JavaScript files.
    - Added script.js to log a welcome message to the console.

--- html/index.php
+++ html/index.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>Hello world!</title>
-    </head>
-    <body>
-        <?php
-        echo"Hello world";
+<html lang="en">
 
-?>
-    </body>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sjabloon</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://cdn.tailwindcss.com/3.4.17"></script>
+</head>
+
+<body>
+
+    <header class="backgroundcolor399DDB flex justifycontentcenter">
+        <h1>Volare Airways</h1>
+        <nav>
+
+        </nav>
+
+    </header>
+
+    <main>
+        
+    </main>
+    <footer>
+
+    </footer>
+    <script src="assets/js/script.js"></script>
+</body>
+
 </html>
