693a87f555a11bf121be8be632fa030afbab8fd2
Morris <1216770@student.roc-nijmegen.nl>
2025-12-11 15:16:28 +0100

    Add loading screen and animation for Volare Airways

--- html/assets/js/script.js
+++ html/assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("Welcome to Volare Airways!")
+console.log("Welcome to Volare Airways!");
+
+document.addEventListener("DOMContentLoaded", () => {
+  showLoadScreen();
+
+  setTimeout(hideLoadScreen, 3000);
+});
+
+function showLoadScreen() {
+  let loadingScreen = document.getElementById("overlay");
+  if (loadingScreen) {
+    loadingScreen.style.display = "inline";
+    loadingScreen.style.opacity = "1";
+  }
+}
+
+function hideLoadScreen() {
+  let loadingScreen = document.getElementById("overlay");
+  if (loadingScreen) {
+    let steps = 100;
+    for (let i = 0; i <= steps; i++) {
+      setTimeout(() => {
+        loadingScreen.style.opacity = ((steps - i) / steps).toString();
+      }, 1 * i);
+    }
+    setTimeout(() => {
+      loadingScreen.style.display = "none";
+    }, 1000 * (steps + 1));
+  }
+}
--- html/index.php
+++ html/index.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sjabloon</title>
+    <title>Volare Airways</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <script src="https://cdn.tailwindcss.com/3.4.17"></script>
 </head>
 
 <body>
-
+    <iframe src="loading.html" id="overlay" class="overlay"></iframe>
     <header class="backgroundcolor399DDB flex justifycontentcenter">
         <h1>Volare Airways</h1>
         <nav>
@@ -20,7 +20,6 @@
     </header>
 
     <main>
-        
     </main>
     <footer>
 
