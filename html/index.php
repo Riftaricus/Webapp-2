7f2367cf7c80a00a3821050246be9d9a4940899b
Rift <morris@dasookwat.com>
2026-01-09 09:30:22 +0100

    Adds database connection and user session logic
    Introduces backend logic for database connectivity, user session initialization, and authentication. Enhances homepage functionality by retrieving random country data and calculating average ratings, laying groundwork for dynamic content and user management.

--- html/index.php
+++ html/index.php
@@ -1,3 +1,123 @@
+<?php
+session_start();
+
+$_SESSION["username"] = "Null";
+$_SESSION["userid"] = "Null";
+$_SESSION["isAdmin"] = false;
+
+?>
+
+
+<?php
+$database = "mysql_db";
+$databaseName = "Flights";
+$username = "VolareWebsite";
+$password = "root";
+
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$database;dbname=$databaseName;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
+];
+
+try {
+    $connect = new PDO($dsn, $username, $password, $opt);
+    echo "Connection succesfully made!";
+} catch (PDOException $e) {
+    echo "" . $e->getMessage() . "";
+}
+breakLine();
+
+echo(getRandomCountry($connect));
+
+login("root", "root", $connect);
+
+breakLine();
+
+echo (getAverageRating($connect));
+
+function getRandomCountry($connection)
+{
+    $min = 0;
+    $max = 9;
+
+    $random = rand($min, $max);
+    $sql = "SELECT * FROM Country WHERE Country_Id = $random";
+    $result = runSQL($sql, $connection);
+
+    return $result[0]['Country_Name'];
+}
+
+function breakLine()
+{
+    echo "<br>";
+}
+
+function getAverageRating($connection)
+{
+    $sql = "SELECT Rating FROM Review";
+
+    $result = runSQL($sql, $connection);
+
+    $averageRating = 0;
+
+    $totalRating = 0;
+
+    $amountOfReviews = 0;
+
+    foreach ($result as $row) {
+        $totalRating += $row['Rating'];
+
+        $amountOfReviews++;
+    }
+
+    $averageRating = $totalRating / $amountOfReviews;
+
+    $averageRating = round($averageRating, 1);
+
+    return $averageRating;
+}
+
+function login($username, $password, $connection)
+{
+    try {
+        $sql = 'SELECT * FROM Account_Data';
+
+        $result = runSQL($sql, $connection);
+
+        foreach ($result as $row) {
+            if ($row['Password'] == $password) {
+                $_SESSION['username'] = $row['Username'];
+                $_SESSION['userid'] = $row['UserId'];
+                if ($row['IsAdmin'] == true) {
+                    $_SESSION['isAdmin'] = true;
+                } else {
+                    $_SESSION['isAdmin'] = false;
+                }
+                return true;
+            }
+        }
+        return false;
+    } catch (Exception $e) {
+        return false;
+    }
+}
+
+function runSQL(string $sql, PDO $connection)
+{
+    $stmt = $connection->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    return $result;
+}
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -9,6 +129,7 @@
 </head>
 
 <body>
+
     <header class="backgroundcolor399DDB flex justifycontentaround flexcolumn">
         <h1>Volare Airways</h1>
         <nav class="flex justifycontentcenter alignitemscenter">
