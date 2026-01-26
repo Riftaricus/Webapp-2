a3e0bcdcafeb3c97b1e212b5e0cc52a53e724021
Rift <morris@dasookwat.com>
2026-01-26 14:12:14 +0100

    Adds booking management functions for flights
    Introduces functions to retrieve, book, and remove booked flights, streamlining database interactions for flight reservations and improving backend modularity.

--- html/assets/php/functions/bookedFlights.php
+++ html/assets/php/functions/bookedFlights.php
@@ -1,10 +1,63 @@
-<?php 
+<?php
 require_once("./connection.php");
+require_once("./flights.php");
 
-function getBookedFlights(){
+function getBookedFlights()
+{
     global $connect;
 
-    $sql = "";
+    $sql = "SELECT * FROM Booked_Flights";
+
+    $stmt = $connect->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+
+    return $result;
+}
+
+function getBookedFlight($id)
+{
+    global $connect;
+    $sql = "SELECT * FROM Booked_Flights WHERE id = :id";
+
+    $stmt = $connect->prepare($sql);
+    $stmt->execute([':id' => $id]);
+    $result = $stmt->fetchAll();
+    return $result;
+}
+
+function removeAllBookedFlightsById($id)
+{
+    global $connect;
+    $sql = 'DELETE FROM Booked_Flights WHERE id = :id';
+    $stmt = $connect->prepare($sql);
+    $stmt->execute([':id' => $id]);
+}
+
+function bookFlight($id)
+{
+    $flight = getFlights($id);
+
+
+
+
+
+
+
+
+
+    global $connect;
+    $sql = 'INSERT INTO Booked_Flights (Flight_Id, Flight_Duration, From_Country_Id, To_Country_Id, UserId)
+    VALUES (:Flight_Id, :Flight_Duration, :From_Country_Id, :To_Country_Id, :UserId)';
+
+    $stmt = $connect->prepare($sql);
+    $stmt->execute([
+        ':Flight_Id' => $flight['Flight_Id'],
+        ':Flight_Duration' => $flight['Flight_Duration'],
+        ':From_Country_Id' => $flight['From_Country_Id'],
+        ':To_Country_Id' => $flight['To_Country_Id'],
+        ':UserId' => $_SESSION['UserId']
+    ]);
 }
 
 ?>
