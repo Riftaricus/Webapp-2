642b064369669ecd7768bbc9f8c23dedf02068f1
M3v_ <max@m3v.dev>
2026-01-29 12:03:24 +0100

    Improves mobile navigation with popdown menu
    Replaces hidden mobile nav with an interactive popdown menu for better accessibility and user experience on small screens. Updates related styles and scripts for smooth transitions and consistent design. Enhances search form UI with icon buttons for a more modern look.

--- html/assets/php/header.php
+++ html/assets/php/header.php
@@ -40,7 +40,7 @@ include("assets/php/menu.php");
                         }
 
                         echo "</select>";
-                        echo '<button type="submit">Search</button>';
+                        echo "<button type='submit' class='header-account-btn'><img class='user-icon' src='assets/img/search.svg' alt='search'></button>";
                         echo '</form>';
                     }
 
@@ -60,7 +60,7 @@ include("assets/php/menu.php");
                         }
 
                         echo "</select>";
-                        echo '<button type="submit">Search</button>';
+                        echo "<button type='submit' class='header-account-btn'><img class='user-icon' src='assets/img/search.svg' alt='search'></button>";
                         echo '</form>';
                     }
                 ?>
@@ -77,67 +77,70 @@ include("assets/php/menu.php");
                 <h1>Volare Airways</h1>
             </a>
 
-            <button type="button" id="header-menu-toggle" class="header-menu-btn">
-                <img class="menu-icon" src="assets/img/menu.svg" alt="menu">
-            </button>
+            <div class="header-menu-container">
+                <button type="button" id="header-menu-toggle" class="header-menu-btn">
+                    <img class="menu-icon" src="assets/img/menu.svg" alt="menu">
+                </button>
+                <div id="popdown-menu">
+                    <nav class="header-nav-small">
+                        <h3 class="header-nav-title">Nav</h3>
+                        <ul class="flex flexcolumn">
+                            <li><a href="/about.php" class="header-nav-link">About</a></li>
+                            <li><a href="/locations.php" class="header-nav-link">Locations</a></li>
+                            <li><a href="/flights.php" class="header-nav-link">Flights</a></li>
+                            <li><a href="/reviews.php" class="header-nav-link">Reviews</a></li>
+                            <li><a href="/contact.php" class="header-nav-link">Contact</a></li>
+                        </ul>
+                    </nav>
+                    <div class="header-actions flex flexrow alignitemscenter gap-25">
+                        <?php
+                        if ($_SERVER['REQUEST_URI'] === "/flights.php") {
+                            echo '<form class="search-wrapper-small" method="post" action="assets/php/forms/searchFlights.php">';
+                            echo '<select id="searchbar" name="searchbar" class="header-search" placeholder="Search Flights...">';
+                            $set = [];
+                            foreach (getFlights() as $flight) {
+                                $set[] = getCountryNameFromId($flight["To_Country_Id"]);
+                                $set[] = getCountryNameFromId($flight["From_Country_Id"]);
+                            }
 
-            <div style="display: none;">
-                <nav class="header-nav">
-                    <ul class="flex flexrow">
-                        <li><a href="/about.php">About</a></li>
-                        <li><a href="/locations.php">Locations</a></li>
-                        <li><a href="/flights.php">Flights</a></li>
-                        <li><a href="/reviews.php">Reviews</a></li>
-                        <li><a href="/contact.php">Contact</a></li>
-                    </ul>
-                </nav>
+                            $set = array_unique($set);
 
-                <div class="header-actions flex flexrow alignitemscenter gap-25">
-                    <?php
-                    if ($_SERVER['REQUEST_URI'] === "/flights.php") {
-                        echo '<form class="search-wrapper" method="post" action="assets/php/forms/searchFlights.php">';
-                        echo '<select id="searchbar" name="searchbar" class="header-search" placeholder="Search Flights...">';
-                        $set = [];
-                        foreach (getFlights() as $flight) {
-                            $set[] = getCountryNameFromId($flight["To_Country_Id"]);
-                            $set[] = getCountryNameFromId($flight["From_Country_Id"]);
+                            foreach ($set as $country) {
+                                echo "<option>" . $country . "</option>";
+                            }
+
+                            echo "</select>";
+                            echo "<button type='submit' class='header-account-btn'><img class='user-icon' src='assets/img/search.svg' alt='search'></button>";
+                            echo '</form>';
                         }
 
-                        $set = array_unique($set);
+                        if (str_contains($_SERVER['REQUEST_URI'], "/locations.php")) {
 
-                        foreach ($set as $country) {
-                            echo "<option>" . $country . "</option>";
+                            echo '<form class="search-wrapper-small" method="post" action="assets/php/forms/searchLocations.php">';
+                            echo '<select id="searchbar" name="searchbar" class="header-search" placeholder="Search Flights...">';
+                            $set = [];
+                            foreach (getCountries() as $country) {
+                                $set[] = $country['Country_Name'];
+                            }
+
+                            $set = array_unique($set);
+
+                            foreach ($set as $country) {
+                                echo "<option>" . $country . "</option>";
+                            }
+
+                            echo "</select>";
+                            echo "<button type='submit' class='header-account-btn'><img class='user-icon' src='assets/img/search.svg' alt='search'></button>";
+                            echo '</form>';
                         }
+                        ?>
 
-                        echo "</select>";
-                        echo '<button type="submit">Search</button>';
-                        echo '</form>';
-                    }
-
-                    if (str_contains($_SERVER['REQUEST_URI'], "/locations.php")) {
-
-                        echo '<form class="search-wrapper" method="post" action="assets/php/forms/searchLocations.php">';
-                        echo '<select id="searchbar" name="searchbar" class="header-search" placeholder="Search Flights...">';
-                        $set = [];
-                        foreach (getCountries() as $country) {
-                            $set[] = $country['Country_Name'];
-                        }
-
-                        $set = array_unique($set);
-
-                        foreach ($set as $country) {
-                            echo "<option>" . $country . "</option>";
-                        }
-
-                        echo "</select>";
-                        echo '<button type="submit">Search</button>';
-                        echo '</form>';
-                    }
-                    ?>
-
-                    <button type="button" id="account-menu-toggle" class="header-account-btn">
-                        <img class="user-icon" src="assets/img/account_icon.png" alt="Account">
-                    </button>
+                        <button type="button" id="account-menu-toggle" class="header-account-btn" style="margin-left:16px;">
+                            <img class="user-icon" src="assets/img/account_icon.png" alt="Account">
+                        </button>
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
 </header>
