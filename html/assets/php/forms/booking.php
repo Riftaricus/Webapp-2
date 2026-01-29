a4882dc29641357d1970b1abefa281da7bef2043
M3v_ <max@m3v.dev>
2026-01-29 13:32:24 +0100

    Adds input validation to booking and transaction forms
    Improves reliability by ensuring required POST parameters
    and session data are present before proceeding with booking
    or transaction logic. Prevents unintended access or errors
    from missing input data.

--- html/assets/php/forms/booking.php
+++ html/assets/php/forms/booking.php
@@ -2,7 +2,7 @@
 require_once("../functions/connection.php");
 require_once("../functions/bookedFlights.php");
 
-if (!isset($_POST['flight_id'])) {
+if (!isset($_POST['flight_id']) ) {
     die('Invalid request');
 }
 
--- html/assets/php/forms/transaction.php
+++ html/assets/php/forms/transaction.php
@@ -4,6 +4,11 @@ require_once "../functions/bookedFlights.php";
 
 $success = 0;
 
+if (!isset($_POST['flightId']) || !isset($_SESSION['username'])) {
+    header("Location: /flights.php?createSuccess=$success");
+	exit;
+}
+
 $success = 1;
 
 bookFlight((int) $_POST['flightId']);
